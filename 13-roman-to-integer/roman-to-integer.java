8ae57a9ec6c9d6216181f6af008a60ac622ac75e
rohan012-sone <rohanwlord@gmail.com>
2026-05-26 11:38:31 +0530

    Time: 2 ms (100.00%) | Memory: 46.4 MB (75.66%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,35 +1,66 @@
 class Solution {
-
-    public int getValue(char ch) {
-
-        switch(ch) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0;
-        }
-    }
-
     public int romanToInt(String s) {
-
-        int sum = 0;
-
-        for(int i = 0; i < s.length(); i++) {
-
-            int current = getValue(s.charAt(i));
-
-            if(i < s.length() - 1 && current < getValue(s.charAt(i + 1))) {
-                sum -= current;
-            }
-            else {
-                sum += current;
-            }
+       int n = s.length();
+       int sum = 0;
+       for(int i=0;i<n;i++){
+        int current = value(s.charAt(i));
+        if(i<n-1 && current<value(s.charAt(i+1))){
+            sum-=current;
+        }else{
+            sum=sum+current;
         }
-
-        return sum;
+       } 
+       return sum;
     }
-}
+    public int value(char c){
+    switch (c){
+        case'I':return 1;
+        case'V':return 5;
+        case'X':return 10;
+        case'L':return 50;
+        case'C':return 100;
+        case'D':return 500;
+        case'M':return 1000;
+    
+        default : return 0;}
+}}
+// class Solution {
+
+//     public int romanToInt(String s) {
+
+//         int n = s.length();
+//         int sum = 0;
+
+//         for(int i = 0; i < n; i++) {
+
+//             int current = value(s.charAt(i));
+
+//             if(i < n - 1 &&
+//                current < value(s.charAt(i + 1))) {
+
+//                 sum -= current;
+//             }
+//             else {
+//                 sum += current;
+//             }
+//         }
+
+//         return sum;
+//     }
+
+//     public int value(char c) {
+
+//         switch(c) {
+
+//             case 'I': return 1;
+//             case 'V': return 5;
+//             case 'X': return 10;
+//             case 'L': return 50;
+//             case 'C': return 100;
+//             case 'D': return 500;
+//             case 'M': return 1000;
+
+//             default: return 0;
+//         }
+//     }
+// }
